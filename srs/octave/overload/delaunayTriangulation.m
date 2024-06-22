299dca01c24353211baf3021d6406d6f9d00cfc2
MarkBFlegg <mark.flegg@monash.edu>
2024-06-23 00:07:03 +1000

    Delete srs/octave directory

--- srs/octave/overload/delaunayTriangulation.m
+++
@@ -1,4 +0,0 @@
-function dt=delaunayTriangulation(x)  % emulate matlab's delaunay 
-t=delaunayn(x); t=elem_fix(t,x); e=freebdHU(x,t); 
-dt.ConnectivityList=t; dt.freeBoundary=e; dt.Points=x; 
-end
--- srs/octave/overload/pause.m
+++
@@ -1,4 +0,0 @@
-function pause
- % input('hit a key'); fprintf('\n');  
- kbhit; 
-endfunction
